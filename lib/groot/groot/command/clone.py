3e38316c769dc2a587bffe5e21ca3f7280bae9d6
Jeremy Slade <jslade@intelepeer.com>
2011-11-10 23:07:28 +0000

    Handle ambiguous situation with directory names that are substrings of another

--- lib/groot/groot/command/clone.py
+++ lib/groot/groot/command/clone.py
@@ -11,6 +11,18 @@ class Clone(BaseCommand):
         
 
     def run(self):
+        self.clone_repo()
+        self.update_submodules()
+        self.checkout_branch()
+
+
+    def clone_repo(self):
         pass
 
 
+    def update_submodules(self):
+        pass
+
+
+    def checkout_branch(self):
+        pass
