f2f20af6a4be8ea98865eeb0568e9e4a8e6e0a39
Jeremy Slade <jslade@intelepeer.com>
2011-12-07 09:24:32 +0000

    Added cherry-pick

---
+++ lib/groot/groot/command/cherry_pick.py
@@ -0,0 +1,12 @@
+
+from base import *
+
+# Is this a useful command for groot?
+# Seems like this is an operation that needs to run in a submodule.
+class CherryPick(AliasedCommand):
+    """ 
+
+    """
+    __aliases__ = ['cherry-pick']
+
+
